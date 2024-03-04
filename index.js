c8d2faa5330c1edf6f82bf09c3c0c780dbc2c619
Josh-Manley <joshmanley45@gmail.com>
2024-03-03 20:31:21 -0700

    updated quesiton prompts for better text location

--- index.js
+++ index.js
@@ -29,7 +29,18 @@ const shapeQuestion = [{
     type: 'input',
     message: 'What color would you like the text to be?',
     name: 'shapeTextColor'
-}]
+},
+{
+    type: 'input',
+    message: 'Type an "X" coordinate for text',
+    name: 'textXlocation'
+},
+{
+    type: 'input',
+    message: 'Type a "Y" coordinate for text',
+    name: 'textYlocation'
+}
+]
 
 inquirer.prompt(initialQuestion).then((answer) => {
     //console.log(answer);
@@ -51,23 +62,21 @@ function addShape() {
              newShape = new Square(answer.shapeColor);
         } else {
              newShape = new Triangle(answer.shapeColor);
-
         }
 
         newShape.setColor(answer.shapeColor);
         const shapeElement = newShape.render();
-        const shapeText = newShape.textAttribute();
+        // const shapeText = newShape.textAttribute();
 
         const svgData =
             `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
             ${shapeElement}
 
-            <text ${shapeText} font-size="60" text-anchor="middle" fill="${answer.shapeTextColor}">${answer.shapeText}</text>
+            <text x="${answer.textXlocation}" y="${answer.textYlocation}" font-size="60" text-anchor="middle" fill="${answer.shapeTextColor}">${answer.shapeText}</text>
 
             </svg>`;
 
             fs.writeFileSync('logo.svg', svgData);
-
     })
 }
